575d7f31eef1c3d9f9ea628e5aec377c57626b44
zhangpp <847170934@qq.com>
2017-04-19 13:13:09 +0800

    框架修改

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,8 @@ from django.conf import settings
 from django.contrib.auth import logout, login, authenticate
 from django.core.paginator import Paginator, InvalidPage, EmptyPage, PageNotAnInteger
 from django.contrib.auth.hashers import make_password
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
 from django.db import connection
 from django.db.models import Count
 from blog.models import *
@@ -50,67 +52,51 @@ def global_setting(request):
     return locals()
 
 
-def index(request):
-    try:
-        keywords = request.GET.get('keywords', '').strip()
-        # 最新文章数据
+class IndexListView(ListView):
+    model = Article
+    template_name = "blog_index.html"
+
+    def get_queryset(self):
+        queryset = super(IndexListView, self).get_queryset()
+        keywords = self.request.GET.get('keywords', '').strip()
+        self._keywords = keywords
         if keywords:
-            article_list = getPage(request, Article.objects.filter(title__icontains=keywords))
-        else:
-            article_list = getPage(request, Article.objects.all())
-        #文章归档
-        #1、先要去获取到文章中有的年份-月份
-        #Article.objects.values('date_publish').distinct()
-        # cursor = connection.cursor()
-        # cursor.execute("SELECT DISTINCT DATE_FORMAT(date_publish, '%Y-%m') as col_date FROM blog_article ORDER BY date_publish")
-        # row = cursor.fetchall()
-        # print row
+            queryset = queryset.filter(title__icontains=keywords)
+        return queryset
 
-    except Exception as e:
-        pass
-    return render(request, 'blog_index.html', locals())
+    def get_context_data(self, **kwargs):
+        context = super(IndexListView, self).get_context_data(**kwargs)
+        context['keywords'] = self._keywords
+        return context
+
+class CategoryListView(ListView):
+    model = Article
+    template_name = "blog_category.html"
 
 
-def archive(request):
-    try:
-        #现去获取客户端提交的信息
-        year = request.GET.get('year', None)
-        month = request.GET.get('month', None)
-        article_list = getPage(request, Article.objects.filter(date_publish__icontains=year+'-'+month))
-    except Exception as e:
-        pass
-    return render(request, 'archive.html', locals())
+    def get_queryset(self):
+        queryset = super(CategoryListView, self).get_queryset()
+        category_id = self.request.GET.get('category', None)
+        category_name = ""
+        if category_id:
+            queryset = queryset.filter(category=category_id)
+            category_name = Category.objects.get(id=category_id)
+        self._ctg = category_name
+        return queryset
 
-def tag(request):
-    try:
-        #现去获取客户端提交的信息
-        tag_id = request.GET.get('tag', None)
-        article_list = getPage(request, Article.objects.filter(tag=tag_id))
-        tag_name = Tag.objects.get(id=tag_id)
-    except Exception as e:
-        pass
-    return render(request, 'tag.html', locals())
+    def get_context_data(self, **kwargs):
+        context = super(CategoryListView, self).get_context_data(**kwargs)
+        context['category_name'] = self._ctg
+        return context
 
-def category(request):
-    try:
-        #现去获取客户端提交的信息
-        category_id = request.GET.get('category', None)
-        article_list = getPage(request, Article.objects.filter(category=category_id))
-        category_name = Category.objects.get(id=category_id)
-    except Exception as e:
-        pass
-    return render(request, 'blog_category.html', locals())
+class ArticleDetailView(UpdateView):
+    model = Article
+    template_name = "blog_article.html"
 
 
-#分页代码
-def getPage(request, article_list):
-    paginator = Paginator(article_list, 5)
-    try:
-        page = int(request.GET.get('page', 1))
-        article_list = paginator.page(page)
-    except (EmptyPage, InvalidPage, PageNotAnInteger):
-        article_list = paginator.page(1)
-    return article_list
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        return context
 
 #文章详情
 def article(request):
@@ -125,111 +111,94 @@ def article(request):
             )
         except Article.DoesNotExist:
             return render(request, 'failure.html', {'reason': '没有找到相对应的文章'})
-            # 评论表单
-        comment_form = CommentForm({'author': request.user.username,
-                                    'email': request.user.email,
-                                    'url': request.user.url,
-                                    'article': id} if request.user.is_authenticated() else{'article': id})
-        # 获取评论信息
-        comments = Comment.objects.filter(article=article).order_by('id')
-        comment_list = []
-        for comment in comments:
-            for item in comment_list:
-                if not hasattr(item, 'children_comment'):
-                    setattr(item, 'children_comment', [])
-                if comment.pid == item:
-                    item.children_comment.append(comment)
-                    break
-            if comment.pid is None:
-                comment_list.append(comment)
 
     except Exception as e:
         print(e)
     return render(request, 'blog_article.html', locals())
 
-#提交评论
-def comment_post(request):
-    try:
-        comment_form = CommentForm(request.POST)
-        if comment_form.is_valid():
-            #获取表单信息
-            comment = Comment.objects.create(
-                username=comment_form.cleaned_data["author"],
-                email=comment_form.cleaned_data["email"],
-                content=comment_form.cleaned_data["comment"],
-                article_id=comment_form.cleaned_data["article"],
-                user=request.user if request.user.is_authenticated() else None
-            )
-            comment.save()
-        else:
-            return render(request, 'failure.html', {'reason': comment_form.errors})
-    except Exception as e:
-        print(e)
-    return redirect(request.META['HTTP_REFERER'])
-
-def do_login(request):
-    try:
-        if request.method == 'POST':
-            login_form = LoginForm(request.POST)
-            if login_form.is_valid():
-                # 登录
-                username = login_form.cleaned_data["username"]
-                password = login_form.cleaned_data["password"]
-                user = authenticate(username=username, password=password)
-                if user is not None:
-                    user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
-                    login(request, user)
-                else:
-                    return render(request, 'failure.html', {'reason': '登录验证失败'})
-                return redirect(request.POST.get('source_url'))
-            else:
-                return render(request, 'failure.html', {'reason': login_form.errors})
-        else:
-            login_form = LoginForm()
-    except Exception as e:
-        print(e)
-    return redirect(request.META['HTTP_REFERER'])
-
-# 注销
-def do_logout(request):
-    try:
-        logout(request)
-    except Exception as e:
-        print(e)
-    return redirect(request.META['HTTP_REFERER'])
-
-# 注册
-def do_reg(request):
-    try:
-        if request.method == 'POST':
-            reg_form = RegForm(request.POST)
-            user_list = User.objects.all()
-            username_list = []
-            for user in user_list:
-                username_list.append(user.username)
-            if reg_form.is_valid():
-                if reg_form.cleaned_data["password"] != reg_form.cleaned_data["repassword"]:
-                    return render(request, 'failure.html', {'reason': '前后密码不一致！'})
-                if reg_form.cleaned_data["username"] in username_list:
-                    return render(request, 'failure.html', {'reason': '该用户名已存在！'})
-                # 注册
-                user = User.objects.create(username=reg_form.cleaned_data["username"],
-                                    email=reg_form.cleaned_data["email"],
-                                    url=reg_form.cleaned_data["url"],
-                                    password=make_password(reg_form.cleaned_data["password"]),)
-                user.save()
-
-                # 登录
-                user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
-                login(request, user)
-                return redirect(request.POST.get('source_url'))
-            else:
-                return render(request, 'failure.html', {'reason': reg_form.errors})
-        else:
-            reg_form = RegForm()
-    except Exception as e:
-        return render(request, 'failure.html', {'reason': e})
-    return render(request, 'blog_register.html', locals())
+# #提交评论
+# def comment_post(request):
+#     try:
+#         comment_form = CommentForm(request.POST)
+#         if comment_form.is_valid():
+#             #获取表单信息
+#             comment = Comment.objects.create(
+#                 username=comment_form.cleaned_data["author"],
+#                 email=comment_form.cleaned_data["email"],
+#                 content=comment_form.cleaned_data["comment"],
+#                 article_id=comment_form.cleaned_data["article"],
+#                 user=request.user if request.user.is_authenticated() else None
+#             )
+#             comment.save()
+#         else:
+#             return render(request, 'failure.html', {'reason': comment_form.errors})
+#     except Exception as e:
+#         print(e)
+#     return redirect(request.META['HTTP_REFERER'])
+#
+# def do_login(request):
+#     try:
+#         if request.method == 'POST':
+#             login_form = LoginForm(request.POST)
+#             if login_form.is_valid():
+#                 # 登录
+#                 username = login_form.cleaned_data["username"]
+#                 password = login_form.cleaned_data["password"]
+#                 user = authenticate(username=username, password=password)
+#                 if user is not None:
+#                     user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
+#                     login(request, user)
+#                 else:
+#                     return render(request, 'failure.html', {'reason': '登录验证失败'})
+#                 return redirect(request.POST.get('source_url'))
+#             else:
+#                 return render(request, 'failure.html', {'reason': login_form.errors})
+#         else:
+#             login_form = LoginForm()
+#     except Exception as e:
+#         print(e)
+#     return redirect(request.META['HTTP_REFERER'])
+#
+# # 注销
+# def do_logout(request):
+#     try:
+#         logout(request)
+#     except Exception as e:
+#         print(e)
+#     return redirect(request.META['HTTP_REFERER'])
+#
+# # 注册
+# def do_reg(request):
+#     try:
+#         if request.method == 'POST':
+#             reg_form = RegForm(request.POST)
+#             user_list = User.objects.all()
+#             username_list = []
+#             for user in user_list:
+#                 username_list.append(user.username)
+#             if reg_form.is_valid():
+#                 if reg_form.cleaned_data["password"] != reg_form.cleaned_data["repassword"]:
+#                     return render(request, 'failure.html', {'reason': '前后密码不一致！'})
+#                 if reg_form.cleaned_data["username"] in username_list:
+#                     return render(request, 'failure.html', {'reason': '该用户名已存在！'})
+#                 # 注册
+#                 user = User.objects.create(username=reg_form.cleaned_data["username"],
+#                                     email=reg_form.cleaned_data["email"],
+#                                     url=reg_form.cleaned_data["url"],
+#                                     password=make_password(reg_form.cleaned_data["password"]),)
+#                 user.save()
+#
+#                 # 登录
+#                 user.backend = 'django.contrib.auth.backends.ModelBackend' # 指定默认的登录验证方式
+#                 login(request, user)
+#                 return redirect(request.POST.get('source_url'))
+#             else:
+#                 return render(request, 'failure.html', {'reason': reg_form.errors})
+#         else:
+#             reg_form = RegForm()
+#     except Exception as e:
+#         return render(request, 'failure.html', {'reason': e})
+#     return render(request, 'blog_register.html', locals())
 
 
 def about(request):
