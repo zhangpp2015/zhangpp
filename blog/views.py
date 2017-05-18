a134fcc1928adba0c87af68d202d4391656c5881
zhangpp <847170934@qq.com>
2017-05-18 17:01:10 +0800

    log

--- blog/views.py
+++ blog/views.py
@@ -299,30 +299,4 @@ def wechat(request):
 
         if not wechat_instance.check_signature(signature=signature, timestamp=timestamp, nonce=nonce):
             return HttpResponseBadRequest('Verify Failed')
-        else:
-            if request.method == 'GET':
-                response = request.GET.get('echostr', 'error')
-            else:
-                try:
-                    wechat_instance.parse_data(request.body)
-                    message = wechat_instance.get_message()
-                    if isinstance(message, TextMessage):
-                        reply_text = 'text'
-                    elif isinstance(message, VoiceMessage):
-                        reply_text = 'voice'
-                    elif isinstance(message, ImageMessage):
-                        reply_text = 'image'
-                    elif isinstance(message, LinkMessage):
-                        reply_text = 'link'
-                    elif isinstance(message, LocationMessage):
-                        reply_text = 'location'
-                    elif isinstance(message, VideoMessage):
-                        reply_text = 'video'
-                    elif isinstance(message, ShortVideoMessage):
-                        reply_text = 'shortvideo'
-                    else:
-                        reply_text = 'other'
-                    response = wechat_instance.response_text(content=reply_text)
-                except ParseError:
-                    return HttpResponseBadRequest('Invalid XML Data')
-            return HttpResponse(response, content_type="application/xml")
+        return HttpResponse('hello world')
