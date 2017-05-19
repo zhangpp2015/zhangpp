d84bb5c6daf735187a1b2f34a0e9af3db48cec1d
zhangpp <847170934@qq.com>
2017-05-19 13:32:30 +0800

    微信测试

--- blog/views.py
+++ blog/views.py
@@ -294,14 +294,11 @@ def wechat(request):
             check_signature(WECHAT_TOKEN, signature, timestamp, nonce)
         except InvalidSignatureException:
             echo_str = 'error'
-        msg = parse_message(request.body)
-        if msg.type == 'text':
-            reply = TextReply(content='测试测试111111', message='1234567890')
-        else:
-            reply = create_reply('Sorry, can not handle this for now', msg)
-        return reply.render()
+        response = HttpResponse(echo_str, content_type="text/plain")
+        return response
     else:
         msg = parse_message(request.body)
+        logger.info(msg)
         if msg.type == 'text':
             reply = TextReply(content='测试测试', message='1234567')
         else:
