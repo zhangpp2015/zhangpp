91197b7e7569e2e49fa0d6a3aa1462ab6b87456a
zhangpp <847170934@qq.com>
2017-05-22 15:12:10 +0800

    微信测试

--- blog/views.py
+++ blog/views.py
@@ -286,7 +286,7 @@ ENCODING_AES_KEY = 'J7HgzQO0n5HTBs6xtMhlYd6I8VrBt86k1aTO6WIhesL'
 
 @csrf_exempt
 def wechat(request):
-    menuCreate()
+    menuGet()
     if request.method == 'GET':
         signature = request.GET.get('signature', '')
         timestamp = request.GET.get('timestamp', '')
@@ -312,39 +312,7 @@ def wechat(request):
     else:
         logger.info('--------------------------------')
 
-def menuCreate():
+def menuGet():
     client = WeChatClient(APP_ID, APP_SECRET)
-    client.menu.create({
-        "button": [
-            {
-                "type": "click",
-                "name": "今日歌曲",
-                "key": "V1001_TODAY_MUSIC"
-            },
-            {
-                "type": "click",
-                "name": "歌手简介",
-                "key": "V1001_TODAY_SINGER"
-            },
-            {
-                "name": "菜单",
-                "sub_button": [
-                    {
-                        "type": "view",
-                        "name": "搜索",
-                        "url": "http://www.soso.com/"
-                    },
-                    {
-                        "type": "view",
-                        "name": "视频",
-                        "url": "http://v.qq.com/"
-                    },
-                    {
-                        "type": "click",
-                        "name": "赞一下我们",
-                        "key": "V1001_GOOD"
-                    }
-                ]
-            }
-        ]
-    })
+    menu = client.menu.get()
+    logger.info(menu)
