d386be32960f61b8e2da7078c51fdff454fad665
zhangpp <847170934@qq.com>
2017-05-22 16:28:33 +0800

    微信测试

--- blog/views.py
+++ blog/views.py
@@ -325,26 +325,6 @@ def createMenu():
                 "name": "歌手简介",
                 "key": "V1001_TODAY_SINGER"
             },
-            {
-                "name": "菜单",
-                "sub_button": [
-                    {
-                        "type": "view",
-                        "name": "搜索",
-                        "url": "http://www.soso.com/"
-                    },
-                    {
-                        "type": "view",
-                        "name": "视频",
-                        "url": "http://v.qq.com/"
-                    },
-                    {
-                        "type": "click",
-                        "name": "赞一下我们",
-                        "key": "V1001_GOOD"
-                    }
-                ]
-            }
         ]
     })
     logger.info(result)
